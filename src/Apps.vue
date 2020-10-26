7094d102f379c987cd5c00df3882eb8bf6bc599f
SheMortal <mortalshe@gmail.com>
2020-10-26 14:09:23 +0200

    Slots

---
+++ src/Apps.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <form-helper></form-helper>
+  </div>
+</template>
+
+<script>
+import formHelper from "./components/formHelper";
+
+export default {
+  components:{
+    'form-helper': formHelper
+  },
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
